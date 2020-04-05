195e1a9a212601487d8fa9419eda8e1e46bf9be8
dante <c.dante.federici@gmail.com>
2020-04-04 23:41:45 -0400

    Way too much fun making a dumb window.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,36 +1,60 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { memo, useRef, useState, useCallback, useEffect } from 'react';
 import fourier from 'fourier';
 import fp from 'lodash/fp';
 
-// const arr = new Array()
-const stdlib = {
-	Math: Math,
-	Float32Array: Float32Array,
-	Float64Array: Float64Array,
+interface Point {
+	x: number;
+	y: number;
+}
+
+const getEventPosition = (e: any): Point => {
+	if (e?.touches?.[0]) {
+		return {
+			x: e.touches[0].clientX,
+			y: e.touches[0].clientY,
+		}
+	}
+
+	return { x: e.clientX, y: e.clientY };
+};
+
+const getElementPosition = (elt: HTMLElement): Point => {
+	const rect = elt.getBoundingClientRect();
+	return { x: rect.left, y: rect.top };
 };
 
 const test = (): void => {
-	console.log(fourier);
+	const len = 16;
+	const real = new Float64Array(len);
+	const imag = new Float64Array(len);
+	for (let i = 0; i < len; i++) {
+		real[i] = Math.sin(Math.PI/8 * i);
+		imag[i] = 0;
+	}
 
-	// Create heap for the fft data and twiddle factors
-	const heap = fourier.custom.alloc(16, 3);
-
-	// Create instance of FFT runner
-	const fftRunner = fourier.custom.fft_f64_16_asm(stdlib, null, heap);
-	console.debug(fftRunner);
-
-	// Init twiddle factors
+	// Create + init runner and heap
+	const heap = fourier.custom.alloc(len, 3);
+	const fftRunner = fourier.custom[`fft_f64_${len}_asm`](window, null, heap);
 	fftRunner.init();
+
+	// Fill heap
+	fourier.custom.array2heap(real, new Float64Array(heap), len, 0);
+	fourier.custom.array2heap(imag, new Float64Array(heap), len, len);
 
 	// Run transformations
 	fftRunner.transform();
+
+	// Extract back into real/imag
+	fourier.custom.heap2array(new Float64Array(heap), real, len, 0);
+	fourier.custom.heap2array(new Float64Array(heap), imag, len, len);
 }
+test();
 
 const magPlot = (
 	offset: number,
 	scale: number,
-	realArr: number[],
-	imaginaryArr: number[],
+	realArr: Float64Array,
+	imaginaryArr: Float64Array,
 ): Path2D => {
 	const p2 = new Path2D();
 	p2.moveTo(offset, offset);
@@ -51,8 +75,6 @@ const signalPlot = (offset: number, scale: number, signal: number[]): Path2D => 
 };
 
 const render = (canvas: HTMLCanvasElement): void => {
-	console.debug('!', canvas);
-
 	const ctx2d = canvas.getContext('2d');
 	if (ctx2d) {
 		ctx2d.fillStyle = 'black';
@@ -61,14 +83,38 @@ const render = (canvas: HTMLCanvasElement): void => {
 		const scale = 5;
 
 		// Draw our signal
-		const signal = new Array(64).fill(0).map((x, i) => Math.sin(i * (Math.PI / 8)));
-		ctx2d.strokeStyle = 'white';
-		ctx2d.stroke(signalPlot(10, scale, signal));
+		const len = 64;
+		const real = new Float64Array(len);
+		const imag = new Float64Array(len);
+		for (let i = 0; i < len; i++) {
+			real[i] = Math.sin(Math.PI/8 * i);
+			imag[i] = Math.cos(Math.PI/8 * i + Math.PI/2);
+		}
 
-		const [rfft, ifft] = fourier.dft(signal, (new Array(signal.length)).fill(0));
-		console.debug({ rfft, ifft });
+		// Render the input signal
 		ctx2d.strokeStyle = 'white';
-		ctx2d.stroke(magPlot(10, scale, rfft, ifft));
+		ctx2d.stroke(magPlot(10, scale, real, imag));
+
+
+		// Create + init runner and heap
+		const heap = fourier.custom.alloc(len, 3);
+		const fftRunner = fourier.custom[`fft_f64_${len}_asm`](window, null, heap);
+		fftRunner.init();
+
+		// Fill heap
+		fourier.custom.array2heap(real, new Float64Array(heap), len, 0);
+		fourier.custom.array2heap(imag, new Float64Array(heap), len, len);
+
+		// Run transformations
+		fftRunner.transform();
+
+		// Extract back into real/imag
+		fourier.custom.heap2array(new Float64Array(heap), real, len, 0);
+		fourier.custom.heap2array(new Float64Array(heap), imag, len, len);
+
+		// Render the FFT signal
+		ctx2d.strokeStyle = 'white';
+		ctx2d.stroke(magPlot(10, scale, real, imag));
 	}
 };
 
@@ -126,7 +172,7 @@ export const App = (): JSX.Element => {
 	const onCanvas = useCallback(setCanvas, [setCanvas]);
 
 	useEffect(() => {
-		if (canvas) {
+		if (canvas && (fitSize ?? 0) > 0) {
 			if (canvas.width !== fitSize) {
 				canvas.setAttribute('width', `${fitSize}px`);
 				canvas.setAttribute('height', `${fitSize}px`);
@@ -161,19 +207,22 @@ export const App = (): JSX.Element => {
 	}, [canvas, fitSize, weird]);
 
 	useEffect(() => {
-		setFitSize(
+		const updateSize = (): void => setFitSize(
 			closestPowerOfTwo(
 				Math.min(document.body.clientWidth, document.body.clientHeight)
 			)
 		);
-		window.addEventListener('resize', (e) => {
-			setFitSize(
-				closestPowerOfTwo(
-					Math.min(document.body.clientWidth, document.body.clientHeight)
-				)
-			);
-		})
+		window.addEventListener('resize', updateSize);
+		updateSize();
+
+		return () => {
+			window.removeEventListener('resize', updateSize);
+		};
 	}, [setFitSize]);
+
+	const loadImage = (e) => {
+		console.debug(e.target.value);
+	}
 
 	return (
 		<div style={{
@@ -183,9 +232,132 @@ export const App = (): JSX.Element => {
 		}}>
 			<canvas ref={onCanvas} onMouseMove={getWeird} onTouchMove={touchWeird} />
 			<div style={{ height: '1em' }} />
-			<button onClick={() => setWeird(!weird)}>
-				{weird ? 'enough' : 'get weird'}
-			</button>
+			<div style={{
+				display: 'flex',
+				overflow: 'auto',
+				flex: '1 0 auto',
+			}}>
+				<div style={{ marginRight: '1em' }}>
+					<button onClick={() => setWeird(!weird)}>
+						{weird ? 'enough' : 'get weird'}
+					</button>
+				</div>
+			</div>
+			<MemoDragWindow>
+				<MemoImage />
+			</MemoDragWindow>
 		</div>
 	);
 };
+
+const DragWindow = ({ children }): JSX.Element => {
+	const eltRef = useRef();
+	const [dragging, setDragging] = useState<{ origin: Point; start: Point }>();
+	const [hide, setHide] = useState(false);
+	const [position, setPosition] = useState<Point>({ x: 0, y: document.body.clientHeight - 250});
+
+	// Window mouseup clear drag
+	useEffect(() => {
+		const clear = (): void => {
+			if (dragging) {
+				setDragging(undefined);
+			}
+		};
+		window.addEventListener('mouseup', clear);
+		window.addEventListener('touchend', clear);
+		return () => {
+			window.removeEventListener('mouseup', clear);
+			window.removeEventListener('touchend', clear);
+		};
+	}, [dragging]);
+
+	useEffect(() => {
+		if (!dragging) {
+			return fp.noop;
+		}
+
+		const updatePosition = (e: TouchEvent | MouseEvent): void => {
+			e.preventDefault();
+			e.stopImmediatePropagation();
+			const newPosition = getEventPosition(e);
+			newPosition.x = dragging.origin.x + (newPosition.x - dragging.start.x);
+			newPosition.y = dragging.origin.y + (newPosition.y - dragging.start.y);
+			setPosition(newPosition);
+		}
+		window.addEventListener('mousemove', updatePosition);
+		window.addEventListener('touchmove', updatePosition);
+		return () => {
+			window.removeEventListener('mousemove', updatePosition);
+			window.removeEventListener('touchmove', updatePosition);
+		};
+	}, [dragging]);
+
+	return (
+		<div
+			ref={eltRef}
+			style={{
+				display: 'flex',
+				flexDirection: 'column',
+				position: 'absolute',
+				top: `${position.y}px`,
+				left: `${position.x}px`,
+				backgroundColor: '#000000',
+			}}
+		>
+			<div style={{ flex: '1 0 auto', display: 'flex' }}>
+				<button
+					style={{ flex: '1 0 auto' }}
+					onMouseDown={(e) => {
+						e.preventDefault();
+						setDragging({ origin: getElementPosition(eltRef.current), start: getEventPosition(e) })
+					}}
+					onTouchStart={(e) => {
+						e.preventDefault();
+						setDragging({ origin: getElementPosition(eltRef.current), start: getEventPosition(e) })
+					}}
+				>move</button>
+				<button onClick={() => setHide(!hide)}>
+					{hide ? 'Show' : 'Hide'}
+				</button>
+			</div>
+			{!hide && children}
+		</div>
+	);
+};
+const MemoDragWindow = memo(DragWindow);
+
+
+const Image = (width): JSX.Element => {
+	const [canvas, setCanvas] = useState<HTMLCanvasElement>();
+	const [file, setFile] = useState<File>();
+
+	// const loadImage =
+
+	useEffect(() => {
+		let useResult = true;
+		if (file) {
+			createImageBitmap(file).then(res => {
+				console.debug(res);
+			})
+		}
+		return () => {
+			useResult = false;
+		}
+	}, [file]);
+
+	const onCanvas = useCallback(setCanvas, [setCanvas]);
+
+	return (
+		<div>
+			<canvas ref={onCanvas} />
+			<input
+				onChange={(e) => {
+					setFile(e.target.files[0]);
+				}}
+				type='file'
+				accept='image/*'
+			/>
+		</div>
+	);
+};
+const MemoImage = memo(Image);
