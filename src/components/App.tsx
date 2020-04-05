7184ffef991ac32e6354d188edf0af93b067ca51
dante <c.dante.federici@gmail.com>
2020-04-04 23:48:55 -0400

    Drawing an image from files

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -330,16 +330,44 @@ const MemoDragWindow = memo(DragWindow);
 const Image = (width): JSX.Element => {
 	const [canvas, setCanvas] = useState<HTMLCanvasElement>();
 	const [file, setFile] = useState<File>();
+	const [image, setImage] = useState<ImageBitmap>();
+	const [error, setError] = useState();
 
-	// const loadImage =
+	// Ensure we clean up if we change images
+	useEffect(() => {
+		const oldImage = image;
+		return () => {
+			if (oldImage) {
+				oldImage.close();
+			}
+		}
+	}, [image]);
 
+	// If we have an image + context, render it
+	useEffect(() => {
+		if (image && canvas) {
+			console.debug({image, canvas});
+			const ctx2d = canvas.getContext('2d');
+			ctx2d?.drawImage(image, 0, 0);
+		}
+	}, [image, canvas]);
+
+	// If we have a new file, try to load the image
 	useEffect(() => {
 		let useResult = true;
 		if (file) {
-			createImageBitmap(file).then(res => {
-				console.debug(res);
+			createImageBitmap(file).then(imageBmp => {
+				if (useResult) {
+					setImage(imageBmp);
+				} else {
+					imageBmp.close();
+				}
+			}, error => {
+				console.error(error);
+				setError(JSON.stringify(error));
 			})
 		}
+
 		return () => {
 			useResult = false;
 		}
@@ -357,6 +385,7 @@ const Image = (width): JSX.Element => {
 				type='file'
 				accept='image/*'
 			/>
+			{error && (<pre>{error}</pre>)}
 		</div>
 	);
 };
